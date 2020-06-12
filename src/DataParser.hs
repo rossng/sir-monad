ba8acaa7c9bf5bef46693495327b0872514081d4
root <root@golem-windows.localdomain>
2020-06-13 00:27:35 +0200

    Initial parser for data

---
+++ src/DataParser.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module DataParser where
+
+import Control.Applicative
+import Data.Text (Text)
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+newtype Infections = Infections [Int] deriving (Show, Eq)
+
+sc :: Parser ()
+sc = L.space space1 empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+integer :: Parser Int
+integer = lexeme L.decimal
+
+dataParser :: Parser Infections
+dataParser = Infections <$> sepEndBy integer (symbol ",") <* eof
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Test.Hspec
+import Control.Exception (evaluate)
+import DataParser
+import Text.Megaparsec
+import Data.Either (Either(..))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "DataParser.dataParser" $ do
+        it "parses simple input" $ do
+            let parsed = parse dataParser "" "1,2,3"
+            parsed `shouldBe` (Right (Infections [1,2,3]))
+        it "parses with spaces" $ do
+            let parsed = parse dataParser "" "1, 2 ,3"
+            parsed `shouldBe` (Right (Infections [1,2,3]))
+        it "parses with newlines" $ do
+            let parsed = parse dataParser "" "1\n,2,3\n"
+            parsed `shouldBe` (Right (Infections [1,2,3]))
