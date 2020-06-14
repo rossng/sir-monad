72dbef141da29e0203b14b9f4361d3be0be3604a
Ross Gardiner <ross-public@gmx.com>
2020-06-14 20:42:28 +0200

    Tweaks

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import System.Environment
+
 import Model
 import Charting
 
 main :: IO ()
 main = do
-    x <- testInferenceEpidemic 20 20
+    args <- getArgs
+    x <- testInferenceEpidemic (read $ args !! 0) (read $ args !! 1)
     let bP = extractParams beta x 
     let rP = extractParams rho x 
     let gP = extractParams gamma x 
--- src/Statistics.hs
+++ src/Statistics.hs
@@ -3,25 +3,29 @@ module Statistics where
 import           Control.Monad
 import           Control.Monad.Bayes.Class      ( bernoulli
                                                 , MonadSample
+                                                , discrete
                                                 )
 import           Numeric.Log
 import           Statistics.Distribution        ( logProbability )
 import           Statistics.Distribution.Poisson
                                                 ( poisson )
+import qualified Statistics.Distribution.Binomial
+                                               as SB
 
 boolToInt :: Bool -> Int
 boolToInt True  = 1
 boolToInt False = 0
 
 binomial :: MonadSample m => Int -> Double -> m Int
-binomial n p = Prelude.sum <$> replicateM n (boolToInt <$> bernoulli p)
+--binomial n p = Prelude.sum <$> replicateM n (boolToInt <$> bernoulli p)
+binomial n p = discrete $ SB.binomial n p
 
 poissonPdf
-    :: Double
-    ->
+  :: Double
+  ->
   -- | lambda
-       Int
-    ->
+     Int
+  ->
   -- | x 
-       Log Double
+     Log Double
 poissonPdf lambda x = Exp $ logProbability (poisson lambda) x
